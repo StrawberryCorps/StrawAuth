31aff7af84deeecb1ee11a617f55e41f761c19e4
Eclixal <28450187+eclixal@users.noreply.github.com>
2021-04-05 14:40:00 +0200

    structure plugin bungee & spigot

---
+++ src/main/java/bzh/strawberrycorps/auth/StrawBungee.java
@@ -0,0 +1,29 @@
+package bzh.strawberrycorps.auth;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/*
+ * This file StrawBungee is part of a project StrawAuth.
+ * It was created on 05/04/2021 14:33 by Eclixal.
+ * This file as the whole project shouldn't be modify by others without the express permission from StrawAuth author(s).
+ * Also this comment shouldn't get remove from the file. (see Licence)
+ */
+public class StrawBungee extends Plugin {
+
+    public static StrawBungee STRAW;
+
+    @Override
+    public void onEnable() {
+        STRAW = this;
+        long begin = System.currentTimeMillis();
+        getLogger().info("######################## [StrawAuth - " + getDescription().getVersion() + "] #################################");
+
+        getLogger().info("Plugin enabled in "+(System.currentTimeMillis() - begin)+" ms.");
+        getLogger().info("######################## [Seablock - " + getDescription().getVersion() + "] #################################");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/main/java/bzh/strawberrycorps/auth/StrawSpigot.java
@@ -0,0 +1,29 @@
+package bzh.strawberrycorps.auth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ * This file StrawSpigot is part of a project StrawAuth.
+ * It was created on 05/04/2021 14:33 by Eclixal.
+ * This file as the whole project shouldn't be modify by others without the express permission from StrawAuth author(s).
+ * Also this comment shouldn't get remove from the file. (see Licence)
+ */
+public class StrawSpigot extends JavaPlugin {
+
+    public static StrawSpigot STRAW;
+
+    @Override
+    public void onEnable() {
+        STRAW = this;
+        long begin = System.currentTimeMillis();
+        getLogger().info("######################## [StrawAuth - " + getDescription().getVersion() + "] #################################");
+
+        getLogger().info("Plugin enabled in "+(System.currentTimeMillis() - begin)+" ms.");
+        getLogger().info("######################## [Seablock - " + getDescription().getVersion() + "] #################################");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
